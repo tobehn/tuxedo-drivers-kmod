3f8db02c7473060013c2f4b473c06e7eaff9f607
Etienne Schmidt <gladion136@googlemail.com>
2025-02-14 12:46:58 +0100

    feat(tools): support custom kernel build inside build.sh
    Signed-off-by: Etienne Schmidt <gladion136@googlemail.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 # Build script for Tuxedo drivers
 # Builts: tuxedo-drivers-kmod-common, kmod-tuxedo-drivers (for your kernel) and akmod-tuxedo-drivers
 
+# Check if the kernels parameter is provided, if not, default to uname -r
+if [ -z "$1" ]; then
+  kernels=$(uname -r)
+else
+  kernels=$1
+fi
+
 echo "--> Copying spec files to ~/rpmbuild/SPECS/"
 cp ./tuxedo-drivers-kmod.spec ~/rpmbuild/SPECS/
 cp ./tuxedo-drivers-kmod-common.spec ~/rpmbuild/SPECS/
@@ -16,7 +23,7 @@ spectool -g -R tuxedo-drivers-kmod.spec
 echo "--> Building RPMs"
 rpmbuild -ba tuxedo-drivers-kmod-common.spec
 rpmbuild -ba tuxedo-drivers-kmod.spec
-rpmbuild -ba tuxedo-drivers-kmod.spec --define 'kernels $(uname -r)'
+rpmbuild -ba tuxedo-drivers-kmod.spec --define "kernels ${kernels}"
 
 echo "--> Listing RPMs"
 tree ~/rpmbuild/RPMS/
